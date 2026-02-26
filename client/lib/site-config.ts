91387ea0346d2627423f3851c30d41465900c781
Praveen Yadav <praveenyadav7787@gmail.com>
2026-02-26 17:43:45 +0530

    update Contact Us page with dynamic content from site configuration file and fix select element options style.

--- client/lib/site-config.ts
+++ client/lib/site-config.ts
@@ -11,6 +11,7 @@ export const siteConfig = {
     addressLine1: "10640 Northwest Fwy",
     addressLine2: "Houston, TX 77092",
     mapsLink: "https://maps.app.goo.gl/g8QxNj3HQzpDiowU6",
+    googleMapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3461.7446648865675!2d-95.46094889999999!3d29.8139202!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c68a15ae6745%3A0x2732b49113a3b5a!2s10640%20Northwest%20Fwy%2C%20Houston%2C%20TX%2077092%2C%20USA!5e0!3m2!1sen!2sin!4v1771412770511!5m2!1sen!2sin",
     phone: "", // Add if available
     emails: [
       "royden@lankylandtx.com",
